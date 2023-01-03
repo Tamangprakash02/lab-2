06d113705cda30312eeb038206c4eaf89fd2ef60
prakashtamang <tamangprakash02@gmail.com>
2023-01-03 23:05:59 +0545

    overcoming the limitation of foreach statement using for statement

--- program2.cs
+++ program2.cs
@@ -1,6 +1,5 @@
-string[] names = { "Alex", "Eddie", "David", "Michael" };
-foreach (var name in names)
-{
-    // Can't reassign the value of name this:
-    if (name == "David") name = "Sammy";
-}
+string[] names = { "Prakash", "Naruto", "David Bechkam", "Michael jackson" };
+for (int i = 0; i < names.Length; i++)
+    if (names[i] == "David Bechkam") names[i] = "Saint Cazola";
+
+foreach (var name in names) Console.WriteLine(name);
